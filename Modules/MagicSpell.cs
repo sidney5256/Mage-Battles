40fc1ef189e93626d0ff8a242ebb22a8cd36dea0
sidney5256 <sep394@nyu.edu>
2019-07-15 23:28:47 -0400

    Create MagicSpell.cs

---
+++ Modules/MagicSpell.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class MagicSpell{
+
+	public string spellName;
+	public int spellLevel;
+
+	public virtual void UpdateSpell(){
+		
+	}
+
+	public virtual void CastSpell(){
+		
+	}
+
+}
